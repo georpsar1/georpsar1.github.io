bfe6737bbf92800d6044c64db63971d501954115
George Psaroudakis <georpsar1@gmail.com>
2024-06-05 12:51:38 +0300

    changed script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 
-document.addEventListener('DOMContentLoaded', function () {
-    const currentUrl = window.location.pathname;
-    const navLinks = document.querySelectorAll('nav a');
 
-    navLinks.forEach(link => {
-        console.log(link)
-        if (link.href.includes(currentUrl)) {
-            link.parentElement.classList.add('bg-gray-200');
-        }
-    });
+const currentUrl = window.location.pathname;
+const navLinks = document.querySelectorAll('nav a');
 
+navLinks.forEach(link => {
+    console.log(link)
+    if (link.href.includes(currentUrl)) {
+        link.parentElement.classList.add('bg-gray-200');
+    }
 });
+
