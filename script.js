08345764b5d039db905614452de5812e93ec670d
George Psaroudakis <georpsar1@gmail.com>
2024-06-05 13:17:45 +0300

    reverted, caused button to break

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 
+document.addEventListener('DOMContentLoaded', function () {
+    const currentUrl = window.location.pathname;
+    const navLinks = document.querySelectorAll('nav a');
 
-const currentUrl = window.location.pathname;
-const navLinks = document.querySelectorAll('nav a');
+    navLinks.forEach(link => {
+        console.log(link)
+        if (link.href.includes(currentUrl)) {
+            link.parentElement.classList.add('bg-gray-200');
+        }
+    });
 
-navLinks.forEach(link => {
-    console.log(link)
-    if (link.href.includes(currentUrl)) {
-        link.parentElement.classList.add('bg-gray-200');
-    }
 });
-
